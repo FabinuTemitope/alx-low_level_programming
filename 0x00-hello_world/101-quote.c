ec6f5a1cb254e3330d69acb9cef48156ef9bb0db
Fabinu Temitope <fabinutemitope94@gmail.com>
2022-09-08 10:55:14 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,11 +3,12 @@
 
 /**
  * main - Entry point
- * return: Always 1 (Success)
+ * Return: Always 1 (Success)
  */
-int main(void);
+int main(void)
 {
 	write(STDOUT_FILENO, "and that piece of art is useful
 			\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
+
