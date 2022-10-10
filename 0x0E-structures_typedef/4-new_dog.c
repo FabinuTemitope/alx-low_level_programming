015c3e3538bbe1d2d5397a36df24ab29ce9819fa
Fabinu Temitope <fabinutemitope94@gmail.com>
2022-10-10 09:10:44 -0700

    new update on task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,51 +1,12 @@
-#include <stdlib.h>
 #include "dog.h"
+#include <stdlib.h>
 
 /**
  * new_dog - new element of type struct dog
  * @name: name of new dog
  * @age: age of new dog
- * @owner: owner of new dog
  *
  * Return: new struct dog
- * if fails, returns NULL.
- */
-
-char *_copy(char *src)
-{
-	char *ptr;
-	int i, len;
-
-	if (src == NULL)
-	{
-return (NULL);
-	}
-
-	for (len = 0; src[len] != '\0'; len++)
-		;
-
-	ptr = malloc(sizeof(char) * (len + 1));
-
-	if (ptr == NULL)
-	{
-		return (NULL);
-	}
-
-	for (i = 0; src[i] != '\0'; i++)
-	{
-		ptr[i] = src[i];
-	}
-
-	ptr[i] = '\0';
-	return (ptr);
-}
-
-/**
- * ew_dog     - Create a new dog variable
- * @name:        Name of the dog
- * @age:         Age of the dog
- * @owner:       Owner of the dog
- * Return:       Pointer to new dog variable
  * if fails, returns NULL.
  */
 dog_t *new_dog(char *name, float age, char *owner)
