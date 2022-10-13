a211458b296d4d99df1e2f8dea2b7360754c7698
Fabinu Temitope <fabinutemitope94@gmail.com>
2022-10-13 10:47:01 -0700

    update

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -13,7 +13,7 @@ void print_all(const char * const format, ...)
 	char *str;
 	const char t_arg[] = "cifs"
 
-		va_start(valist, format)
+		va_start(valist, format);
 		while (format && format[i])
 		{
 			j = 0;
