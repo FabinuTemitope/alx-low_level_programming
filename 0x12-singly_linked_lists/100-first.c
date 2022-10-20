02258ef23e0db59a110d2a80dbc5b9551ab55f83
Fabinu Temitope <fabinutemitope94@gmail.com>
2022-10-20 07:43:07 -0700

    task done successfully

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void first(void) __attribute__ ((constructor));
+
+/**
+ * first - ...
+ * bmain - function executed before main
+ * Return: no return.
+ */
+
+void first(void)
+{
+	printf("You're beat! and yet, you must allow");
+	printf(",\nI bore my house upon my back!\n");
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,2 @@
+0x12-singly_linked_lists
+containing 7tasks
